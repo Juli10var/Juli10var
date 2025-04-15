7afdb1e029c33e7b466760d492f6a1f55241833f
Juli10var <143466980+Juli10var@users.noreply.github.com>
2025-04-15 15:48:19 -0500

    Calculadora en Bash
    Calculadora basica en Bash

--- Calculadora.sh
+++ Calculadora.sh
@@ -1,1 +1,19 @@
-echo
+echo "Bienvenido a la Calculadora, por favor ingrese los digitos"
+read -p "Ingresa el primer numero:" num1
+read -p "Ingresa el segundo numero:" num2
+        echo "Seleciones la operación de desea realizar"
+        echo "1) sumar"
+        echo "2) restar"
+        read -p "Opción: " opcion
+        case $opcion in
+            1)
+                su=$((num1 + num2))
+                echo "Su operación es: $su"
+                ;;
+            2)
+                re=$((num1 + num2))
+                echo "Su operación es: $re"
+                ;;
+     esac
+
+# 15 de abril de 2025 / Juli10var :)  
